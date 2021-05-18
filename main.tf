2db3b500c065599329ba5f26c3dc8edf97aac3b6
Scott Peterson <sm_peterson@yahoo.com>
2021-05-18 15:29:32 -0500

    cloudtrail config

--- main.tf
+++ main.tf
@@ -2,11 +2,11 @@ provider "aws" {
   region = var.aws_region
 }
 
-provider "random" {}
+#provider "random" {}
 
-resource "random_pet" "table_name" {}
+#resource "random_pet" "table_name" {}
 
-resource "aws_dynamodb_table" "tfc_example_table" {
+#resource "aws_dynamodb_table" "tfc_example_table" {
   name = "${var.db_table_name}-${random_pet.table_name.id}"
 
   read_capacity  = var.db_read_capacity
@@ -17,4 +17,10 @@ resource "aws_dynamodb_table" "tfc_example_table" {
     name = "UUID"
     type = "S"
   }
+}
+
+module "aws_cloudtrail" {
+    source             = "trussworks/cloudtrail/aws"
+    s3_bucket_name     = "cloudtrail-logs"
+    log_retention_days = 15
 }
