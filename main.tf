1c10d6dc575f7f330f9dc1cd0673e47693f24ae9
Scott Peterson <sm_peterson@yahoo.com>
2021-05-19 14:01:26 -0500

    adsf

--- main.tf
+++ main.tf
@@ -6,6 +6,10 @@ provider "random" {}
 
 resource "random_pet" "bucket_name" {}
 
+resource "aws_kms_key" "cloudtrail" {
+  description             = "KMS Key for cloudtrail buckets"
+  deletion_window_in_days = 10
+}
 
 resource "aws_s3_bucket" "cloudtrail_logging" {
   bucket = "${random_pet.bucket_name.id}-logs"
@@ -16,7 +20,7 @@ resource "aws_s3_bucket" "cloudtrail_logging" {
   server_side_encryption_configuration {
     rule {
       apply_server_side_encryption_by_default {
-        kms_master_key_id = aws_kms_key.mykey.arn
+        kms_master_key_id = aws_kms_key.cloudtrail.arn
         sse_algorithm = "aws:kms"
       }
     }
