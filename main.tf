c5eddfb5fffb2ead39ee90bcbccb8c648d14c805
Scott Peterson <sm_peterson@yahoo.com>
2021-05-19 14:04:17 -0500

    asdf

--- main.tf
+++ main.tf
@@ -9,6 +9,9 @@ resource "random_pet" "bucket_name" {}
 resource "aws_kms_key" "cloudtrail" {
   description             = "KMS Key for cloudtrail buckets"
   deletion_window_in_days = 10
+  key_usage               = "ENCRYPT_DECRYPT"
+  is_enabled              = true
+ + enable_key_rotation    = true
 }
 
 resource "aws_s3_bucket" "cloudtrail_logging" {
@@ -36,5 +39,5 @@ resource "aws_s3_bucket_public_access_block" "access_good_1" {
   block_public_acls   = true
   block_public_policy = true
   ignore_public_acls  = true
-  restrict_public_bucket = true
+  restrict_public_buckets = true
 }
