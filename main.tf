7903f588ad8397637b4dcef45408471b7fd579f8
Scott Peterson <sm_peterson@yahoo.com>
2021-05-19 13:58:47 -0500

    asdf

--- main.tf
+++ main.tf
@@ -6,52 +6,31 @@ provider "random" {}
 
 resource "random_pet" "bucket_name" {}
 
-/*resource "aws_dynamodb_table" "tfc_example_table" {
-  name = "${var.db_table_name}-${random_pet.table_name.id}"
 
-  read_capacity  = var.db_read_capacity
-  write_capacity = var.db_write_capacity
-  hash_key       = "UUID"
-
-  attribute {
-    name = "UUID"
-    type = "S"
-  }
-}*/
-module "aws-s3-bucket" {
-  source         = "trussworks/s3-private-bucket/aws"
-  bucket         = "${random_pet.bucket_name.id}-logs"
-  use_account_alias_prefix = false
-  enable_analytics = false
-
-  tags = {
-    Name        = "cloudtrail logs"
-    Environment = "Prod"
-    Description = "Logging bucket for cloudtrail s3 bucket"
-  }
-  resource "aws_s3_bucket" "private_bucket" {
+resource "aws_s3_bucket" "cloudtrail_logging" {
+  bucket = "${random_pet.bucket_name.id}-logs"
   acl = "log-delivery-write"
-}
-}
-
-
-module "aws-s3-bucket1" {
-  source         = "trussworks/s3-private-bucket/aws"
-  bucket         = "${random_pet.bucket_name.id}-clouddtrail"
-  logging_bucket = "${random_pet.bucket_name.id}-logs"
-  use_account_alias_prefix = false
-  enable_analytics = false
-
+  versioning {
+    enabled = true
+  }
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        kms_master_key_id = aws_kms_key.mykey.arn
+        sse_algorithm = "aws:kms"
+      }
+    }
+  }
   tags = {
-    Name        = "cloudtrail bucket"
+    Name        = "My bucket"
     Environment = "Prod"
-    Description = "Cloudtrail bucket for cloudtrail logs"
   }
 }
 
-/*
-module "aws_cloudtrail" {
-    source             = "trussworks/cloudtrail/aws"
-    s3_bucket_name     = "cloudtrail-logs"
-    log_retention_days = 90
-}*/
+resource "aws_s3_bucket_public_access_block" "access_good_1" {
+  bucket = aws_s3_bucket.cloudtrail_logging.id
+  block_public_acls   = true
+  block_public_policy = true
+  ignore_public_acls  = true
+  restrict_public_bucket = true
+}
